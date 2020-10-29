c5035949e9a8e9f0c5c336017ce06fb936fdbca4
virdilokeshna <71276942+virdilokeshna@users.noreply.github.com>
2020-10-29 18:42:25 +0530

    jenkinsfile

--- sonar-project.js
+++ sonar-project.js
@@ -4,6 +4,7 @@ const sonarqubeScanner = require('sonarqube-scanner');
        serverUrl: 'http://sonarqubeprac.eastus2.cloudapp.azure.com:9000/',
        options : {
        'sonar.sources': '.',
+       'sonar.login':'ade5d0cc09d7d8d8795335fee19e8d9b3405accd',
        'sonar.inclusions' : 'src/**' // Entry point of your code
        }
      }, () => {});
