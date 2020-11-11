efdc86e41f4b94e598e226296f689b545b6ff71f
virdilokeshna <71276942+virdilokeshna@users.noreply.github.com>
2020-11-11 15:27:44 +0530

    Update sonar-project.js
    update sonar settings

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 
 const sonarqubeScanner = require('sonarqube-scanner');
      sonarqubeScanner({
-       serverUrl: 'http://sonarqubeprac.eastus2.cloudapp.azure.com:9000/',
+       serverUrl: 'http://sonarqube001.eastus.cloudapp.azure.com/',
        options : {
        'sonar.sources': '.',
        //'sonar.login':'ade5d0cc09d7d8d8795335fee19e8d9b3405accd',
